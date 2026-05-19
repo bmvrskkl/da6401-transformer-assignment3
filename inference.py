6b69061776f4a328679898fcdb5d3d7e7b442296
Balaji Srinivasan <balajisrinivasan@Balajis-MacBook-Pro.local>
2026-05-19 13:49:31 +0530

    Update inference pipeline

--- inference.py
+++ inference.py
@@ -3,8 +3,10 @@ import torch
 from models.transformer import Transformer
 
 from utils import (
+    tokenize_de,
     src_vocab,
-    tgt_vocab
+    tgt_vocab,
+    text_to_tensor
 )
 
 device = torch.device(
@@ -33,3 +35,44 @@ model.load_state_dict(
 model.eval()
 
 print("Model loaded successfully")
+
+sentence = "ein kleines mädchen spielt"
+
+tokens = tokenize_de(sentence)
+
+src = text_to_tensor(
+    tokens,
+    src_vocab
+).unsqueeze(0).to(device)
+
+tgt = torch.tensor([[1]]).to(device)
+
+generated_tokens = []
+
+with torch.no_grad():
+
+    for _ in range(20):
+
+        output = model(src, tgt)
+
+        next_token = output[:, -1, :].argmax(-1)
+
+        token_id = next_token.item()
+
+        if token_id == 2:
+            break
+
+        generated_tokens.append(
+            tgt_vocab.lookup_token(token_id)
+        )
+
+        tgt = torch.cat(
+            [tgt, next_token.unsqueeze(0)],
+            dim=1
+        )
+
+print("\nGerman Input:")
+print(sentence)
+
+print("\nPredicted Translation:")
+print(" ".join(generated_tokens))
